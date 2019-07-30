724a092e5ff1ea62449e167a11b0626ae4f49c30
K. Gofron <makerman4@gmail.com>
2019-07-30 11:38:07 -0400

    Create react-redux skeleton

---
+++ frontend/extralo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+// eventually import store
+// eventually import RootReducer
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Extralo test</h1>, root)
+})
