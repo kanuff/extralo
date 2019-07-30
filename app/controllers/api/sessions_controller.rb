33fcb94f90146860c67f85cdcd09d02e7fd8483a
K. Gofron <makerman4@gmail.com>
2019-07-30 13:19:18 -0400

    Finalize setup and add jQuery

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::SesssionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password],
+    ) 
+    if @user
+      login!(@user)
+      render :show #change to partial TASK FOR POST LUNCH
+    else
+      render json: ["Username/password do not match"], status: 422
+    end
+  end
+
+  def destroy
+    if current_user
+      logout!
+      render json: {}
+    else
+      render json: ["You are not logged in"], status: 404
+    end
+  end
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show # ONLY FOR TESTING
+    @user = User.first
+    render :show
+  end
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
