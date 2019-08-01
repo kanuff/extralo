e0a304a456e5af241e03ee37e22a38d44ff963f2
App Academy Student <makerman4@gmail.com>
2019-08-01 17:50:16 -0400

    Refactor user-navbar to make data-flow more clear

--- frontend/components/user_navbar/user_navbar.jsx
+++ frontend/components/user_navbar/user_navbar.jsx
@@ -6,56 +6,14 @@ import UserOptionsMenu from './user_options_menu'
 export default class UserNavbar extends React.Component{
   constructor(props){
     super(props)
-    this.state = {
-      "form-visible": false,
-      "primary-form": "user-options"
-    };
-    this.user_options = "user-options";
-    this.create_board_options = "create-board-options";
-    this.information = "information";
-    this.notifications = "notifications";
-    this.hideForm = this.hideForm.bind(this);
-    this.handleClick = this.handleClick.bind(this);
   }
-
-  componentDidMount(){
-    // document.addEventListener("mousedown", this.handleClick, false)
-  }
-
 
   componentWillUnmount(){
-    // document.removeEventListener("mousedown", this.handleClick, false)
     this.props.closeModal();
   }
 
-  handleClick(e){
-    const allowedClicks = ["BUTTON", "LI", "LINK", "I", "A"]
-    if(!allowedClicks.includes(e.target.nodeName)) {
-      this.hideForm();
-    }
-  }
-
-  showDropdown(){}
-
   showForm(field){
-    return () => {
-      if(!this.state["form-visible"]){
-        this.setState(
-          {
-            "form-visible": true,
-            "primary-form": field 
-          }
-        )
-      } else if(this.state["primary-form"]===field){
-        this.setState(
-          {"form-visible": !this.state["form-visible"]}
-        )
-      } else {
-        this.setState(
-          {"primary-form": field}
-        )
-      }
-    }
+    return () => this.props.openModal(field)
   }
 
   hideForm(){
@@ -65,19 +23,6 @@ export default class UserNavbar extends React.Component{
   }
 
   render(){
-    let form;
-    if(this.state["form-visible"]){
-      switch (this.state["primary-form"]) {
-        case this.user_options:
-          form = <UserOptionsMenu/>
-        // case this.create_board_options:
-        // case this.notifications:
-        // case this.information:
-        default:
-          form
-      }
-    }
-
     return(
       <section className={"user-navbar"}>
         <div className={"float-left"}>
@@ -92,10 +37,8 @@ export default class UserNavbar extends React.Component{
           <button onClick={this.showForm("create-board-options")} className={"create-board-btn"}><i className="fas fa-plus"></i></button>
           <button onClick={this.showForm("information")} className={"information-btn"}><i className="fas fa-info-circle"></i></button>
           <button onClick={this.showForm("notifications")} className={"notification-btn"}><i className="far fa-bell"></i></button>
-          {/* <button onClick={this.showForm("user-options")} id={"user-options-btn"}>M</button> */}
-          <button onClick={() => this.props.openModal("user-options")} id={"user-options-btn"}>M</button>
+          <button onClick={this.showForm("user-options")} id={"user-options-btn"}>M</button>
         </div>
-        {form}
       </section>
     )
   }
