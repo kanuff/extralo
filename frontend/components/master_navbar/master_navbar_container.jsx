6c5b6f1fa1588ac0774a15a6d031c71a90565116
App Academy Student <makerman4@gmail.com>
2019-08-01 16:07:28 -0400

    Remove unused files

--- frontend/components/master_navbar/master_navbar_container.jsx
+++
@@ -1,3 +0,0 @@
-import Navbar from '../navbar/navbar_container';
-import UserNavbar from '../user_navbar/user_navbar_container';
-
--- frontend/extralo.jsx
+++ frontend/extralo.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { configureStore } from './store/store'
 import Root from './components/root'
-// import * as SessionAPIUtil from './util/session_api_util'
-// eventually import store
-// eventually import RootReducer
 
 document.addEventListener('DOMContentLoaded', () => {
 
