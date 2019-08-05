ea35202c7c4eca32ea2f585e30ff1e2cf8311e62
App Academy Student <aa@Apps-Mac-mini-35.local>
2019-08-05 09:53:36 -0400

    Fix some cross-browers display issues with scrollbar on board-show page

--- frontend/components/lists/list_index_item.jsx
+++ frontend/components/lists/list_index_item.jsx
@@ -61,15 +61,9 @@ class ListIndexItem extends React.Component{
                     <div>alsdfjlasdjflkajsdfl</div>
                     <div>alsdfjlasdjflkajsdfl</div>
                     <div>alsdfjlasdjflkajsdfl</div>
-                    <div>alsdfjlasdjflkajsdfl</div>
-                    <div>alsdfjlasdjflkajsdfl</div>
-                    <div>alsdfjlasdjflkajsdfl</div>
-                    <div>alsdfjlasdjflkajsdfl</div>
-                    <div>alsdfjlasdjflkajsdfl</div>
                 </ul>
             </li>
         )
-
     }
 }
 
