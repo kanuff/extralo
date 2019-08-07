45bb42019daf5e5c69da5963819d34870212de50
kanuff <makerman4@gmail.com>
2019-08-07 15:30:05 -0400

    Prep for heroku

--- frontend/components/lists/list_index.jsx
+++ frontend/components/lists/list_index.jsx
@@ -15,16 +15,11 @@ export default class ListIndex extends React.Component{
         this.onDragEnd = this.onDragEnd.bind(this);
     }
 
-    componentDidUpdate(prevProps, prevState){
-        console.log("....... PROPS .......")
-        console.log(prevProps)
-        console.log(this.props)
-        console.log("^^^^^ PROPS ^^^^^^^^")
+    componentDidUpdate(prevProps){
         if(prevProps !== this.props){
             const { listOrder } = this.state;
             const oldListOrder = prevProps.listOrder || []
            if (oldListOrder.length !== this.props.length){
-                console.log("This should trigger on creating a new list")
                 this.generateListOrder();
             } else if (listOrder.length === 0) {
                 this.generateListOrder();
@@ -36,17 +31,12 @@ export default class ListIndex extends React.Component{
     generateListOrder(){
         const { lists } = this.props;
         if(lists.length > 0){
-            debugger
             const listOrder = [];
             let current_list = lists.find(list => list.prev_id === null && list.archived===false)
-            debugger
             if ( current_list){
-                debugger
                 listOrder.push(current_list.id)
                 while( current_list.next_id !== null){
-                    debugger
                     current_list = lists.find(list => current_list.next_id === list.id)
-                    debugger
                     listOrder.push(current_list.id)
                 }
                 this.setState({
@@ -99,7 +89,6 @@ export default class ListIndex extends React.Component{
 
         list.next_id = listOrder[destination.index + 1] || 'sentinel'
         list.prev_id = listOrder[destination.index - 1] || 'sentinel'
-        console.log(list)
         list.order_change = true;
         this.props.updateList(list)
     }
@@ -109,7 +98,7 @@ export default class ListIndex extends React.Component{
             <DragDropContext onDragEnd={this.onDragEnd}>
                 <section className={"lists-index-container"}>
                     <Droppable droppableId={`board_${this.props.board_id}`} direction="horizontal">
-                        {(provided, snapshot) => (
+                        {(provided) => (
                             <ul 
                                 className={"lists-index"}
                                 {...provided.droppableProps}
